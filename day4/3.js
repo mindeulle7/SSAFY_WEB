fd9be6ebfd7293cacb722e47fd72aa90d1258341
mindeulle7 <ehdals990405@naver.com>
2024-02-07 09:54:07 +0900

    web

--- day4/3.js
+++
@@ -1,7 +0,0 @@
-const arr = [1,2,3,4,5,6];
-const newArr = arr.map((data) => data + 1);
-
-arr1 = arr.map(li => li*li);
-
-const abc = ["a", "bcd", "ef", "g"];
-const arr2 = abc.map(li => li.length);
